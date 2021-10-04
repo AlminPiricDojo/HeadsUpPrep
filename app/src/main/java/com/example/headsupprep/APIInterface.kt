985efaad540a7fd669c466e75484042fcc64de46
TMDStudios <tmdstudioscontact@gmail.com>
2021-10-04 03:01:51 -0400

    Removed Headers from APIInterface

--- app/src/main/java/com/example/headsupprep/APIInterface.kt
+++ app/src/main/java/com/example/headsupprep/APIInterface.kt
@@ -5,24 +5,19 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface APIInterface {
-    @Headers("Content-Type: application/json")
     @GET("/celebrities/")
     fun getCelebrities(): Call<ArrayList<Celebrity>>
 
-    @Headers("Content-Type: application/json")
     @POST("/celebrities/")
     fun addCelebrity(@Body celebrityData: Celebrity): Call<Celebrity>
 
-    @Headers("Content-Type: application/json")
     @GET("/celebrities/{id}")
     fun getCelebrity(@Path("id") id: Int): Call<Celebrity>
 
     // PUT replaces the full object (use PATCH to change individual fields)
-    @Headers("Content-Type: application/json")
     @PUT("/celebrities/{id}")
     fun updateCelebrity(@Path("id") id: Int, @Body celebrityData: Celebrity): Call<Celebrity>
 
-    @Headers("Content-Type: application/json")
     @DELETE("/celebrities/{id}")
     fun deleteCelebrity(@Path("id") id: Int): Call<Void>
 }
