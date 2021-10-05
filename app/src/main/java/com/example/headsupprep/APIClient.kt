582413f1a4f5b31fdbb6938505af8d86be745e91
TMDStudios <tmdstudioscontact@gmail.com>
2021-10-05 07:48:47 -0400

    Removed Interceptor

--- app/src/main/java/com/example/headsupprep/APIClient.kt
+++ app/src/main/java/com/example/headsupprep/APIClient.kt
@@ -1,7 +1,5 @@
 package com.example.headsupprep
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,13 +7,9 @@ class APIClient {
     private lateinit var retrofit: Retrofit
 
     fun getClient(): Retrofit{
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
         retrofit = Retrofit.Builder()
             .baseUrl("https://dojo-recipes.herokuapp.com/")
             .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
             .build()
         return retrofit
     }
