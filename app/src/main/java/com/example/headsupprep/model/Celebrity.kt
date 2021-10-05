df009012f3d6586d5c84d5a83bf83fdb5c455b8a
TMDStudios <tmdstudioscontact@gmail.com>
2021-10-05 07:49:38 -0400

    Changed to data class

--- app/src/main/java/com/example/headsupprep/model/Celebrity.kt
+++ app/src/main/java/com/example/headsupprep/model/Celebrity.kt
@@ -1,29 +1,3 @@
 package com.example.headsupprep.model
 
-import com.google.gson.annotations.SerializedName
-
-//data class Celebrity(val name: String, val taboo1: String, val taboo2: String, val taboo3: String)
-class Celebrity{
-    @SerializedName("name")
-    var name: String = ""
-
-    @SerializedName("taboo1")
-    var taboo1: String = ""
-
-    @SerializedName("taboo2")
-    var taboo2: String = ""
-
-    @SerializedName("taboo3")
-    var taboo3: String = ""
-
-    @SerializedName("pk")
-    var pk: Int = 0
-
-    constructor(name: String, taboo1: String, taboo2: String, taboo3: String, pk: Int) {
-        this.name = name
-        this.taboo1 = taboo1
-        this.taboo2 = taboo2
-        this.taboo3 = taboo3
-        this.pk = pk
-    }
-}
+data class Celebrity(val name: String, val taboo1: String, val taboo2: String, val taboo3: String, val pk: Int)
